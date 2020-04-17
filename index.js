2cb444df623226231c4ccffd5ed3b37a73042853
simran <simrankaushal2016@gmail.com>
2020-04-17 22:01:55 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express();
+var exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT =process.env.PORT || 5000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello this is other stuff!";
+app.get('/', function (req, res) {
+    res.render('home',{
+    	stuff:otherstuff
+    });
+});
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT,()=>console.log('Server listenning on port'));
